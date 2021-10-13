f15043de09acd0b000f9cde3811e1b3ab117c5c4
nicksorenson <nicksorenson99@gmail.com>
2021-10-12 23:20:12 -0600

    Starting assignment 2. Need to fix some warnings :D

---
+++ sortutils.hpp
@@ -0,0 +1,16 @@
+// Header file for assignment 4 :D
+const std::size_t HOW_MANY_ELEMENTS = 250000;
+const std::uint8_t HOW_MANY_TIMES = 25;
+using SourceArray = stad::array<int, HOW_MANY_ELEMENTS>;
+void initializeRawArrayFromStdArray(const SourceArray &source, int dest[]);
+void orgainPipeStdArray(SourceArray &data);
+void evaluateRawArray(const SourceArray &random, const SourceArray &sorted,
+                      const SourceArray &sorted, const SourceArray &reversed,
+                      const SourceArray &organPipe, const SourceArray &rotated);
+void evaluateStdArray(const SourceArray &random, const SourceArray &sorted,
+                      const SourceArray &sorted, const SourceArray &reversed,
+                      const SourceArray &organPipe, const SourceArray &rotated);
+void evaluateStdVector(const SourceArray &random, const SourceArray &sorted,
+                       const SourceArray &sorted, const SourceArray &reversed,
+                       const SourceArray &organPipe,
+                       const SourceArray &rotated);
